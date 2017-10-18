598467920e829b5b164ea25869b8fa90d1717478
Ramesh <ramesh4sr@gmail.com>
2017-10-17 21:59:49 -0400

    PermCheck Solved

---
+++ src/main/java/com/rameshify/practice/codility/PermCheck.java
@@ -0,0 +1,27 @@
+package com.rameshify.practice.codility;
+
+import java.util.Arrays;
+
+public class PermCheck {
+    public static void main(String[] args) {
+        System.out.println(new PermCheck().solution(new int[]{4, 1, 3, 2}));
+    }
+
+    int solution(int A[]) {
+        boolean notAPerm = false;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > A.length) {
+                notAPerm = true;
+                break;
+            }
+            if (A[Math.abs(A[i]) - 1] < 0) {
+                notAPerm = true;
+                break;
+            } else {
+                A[Math.abs(A[i]) - 1] = -A[Math.abs(A[i]) - 1];
+            }
+        }
+        return notAPerm ? 0 : 1;
+    }
+}
